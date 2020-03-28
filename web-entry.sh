f22a877f916b961fb09103b718d37f34b73a34b0
Justin Backman <github@jitonline.net>
2020-03-28 08:47:55 -0400

    Create web-entry.sh

---
+++ web-entry.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+action=""
+
+while getopts ":dn:" option
+do
+ case "${option}" in
+ n) NAME=$OPTARG;;
+ d) action="debug";;
+ esac
+done
+export NAME
+if [ "$action" == "debug" ]; then
+    echo "Running in Debug mode"
+    export FLASK_APP=web.py
+    export FLASK_ENV=development
+    flask run --host 0.0.0.0 --port 8080
+else
+    echo "Running in production mode"
+    exec /opt/bb/python/bin/gunicorn -w 4 -b 0.0.0.0:8080 web:app
+fi
