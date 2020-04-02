c3f417ab550130b0a7de224efd584543190d495d
Justin Backman <github@jitonline.net>
2020-04-02 11:49:56 -0400

    Update web.py

--- web.py
+++ web.py
@@ -119,7 +119,7 @@ class whois(Resource,):
     try:
       domain = whois_query.query(whois_name)
       retval = { 'registrar': domain.registrar, 
-                 'name_servers': str(domain.name_servers).replace('{','[').replace('}',']'),
+                 'name_servers': list(domain.name_servers),
                  'status': domain.status,
                  'name': domain.name, 
                }
